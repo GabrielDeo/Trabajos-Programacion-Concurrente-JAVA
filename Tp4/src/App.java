a6b0e38ea3932958e8de37740dfb60cac17d4550
DESKTOP-3ARHIF8\Yo <gabriel0rtega4531@gmail.com>
2024-09-24 19:36:43 -0300

    creando clases

--- Tp4/src/App.java
+++ Tp4/src/App.java
@@ -1,5 +1,9 @@
+    public static void main(String[] args) throws Exception {
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+    }
+}
+
     }
 }
